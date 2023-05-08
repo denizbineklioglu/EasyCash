242f13c395fd440a7ca69b17efec6599d6ef3583
Deniz Bineklioğlu <76698070+denizbineklioglu@users.noreply.github.com>
2023-05-08 11:32:07 +0300

    repositories

---
+++ EasyCash.DataAccess/Abstract/ICustomerAccountProcessDal.cs
@@ -0,0 +1,13 @@
+﻿using EasyCash.Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCash.DataAccess.Abstract
+{
+    public interface ICustomerAccountProcessDal : IGenericDal<CustomerAccountProcess>
+    {
+    }
+}
