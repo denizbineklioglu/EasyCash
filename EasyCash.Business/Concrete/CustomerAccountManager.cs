232a530f202d6a209ffec794865887c69dab8f9e
Deniz Bineklioğlu <76698070+denizbineklioglu@users.noreply.github.com>
2023-05-10 16:01:36 +0300

    temalar

---
+++ EasyCash.Business/Concrete/CustomerAccountManager.cs
@@ -0,0 +1,47 @@
+﻿using EasyCash.Business.Abstract;
+using EasyCash.DataAccess.Abstract;
+using EasyCash.Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCash.Business.Concrete
+{
+    public class CustomerAccountManager : ICustomerAccountService
+    {
+
+        private readonly ICustomerAccountDal _customerAccountDal;
+
+        public CustomerAccountManager(ICustomerAccountDal customerAccountDal)
+        {
+            _customerAccountDal = customerAccountDal;
+        }
+
+        public void TDelete(CustomerAccount t)
+        {
+            _customerAccountDal.Delete(t);
+        }
+
+        public CustomerAccount TGetByID(int id)
+        {
+            return _customerAccountDal.GetByID(id);
+        }
+
+        public List<CustomerAccount> TGetList()
+        {
+            return _customerAccountDal.GetList();
+        }
+
+        public void TInsert(CustomerAccount t)
+        {
+            _customerAccountDal.Insert(t);
+        }
+
+        public void TUpdate(CustomerAccount t)
+        {
+            _customerAccountDal.Update(t);
+        }
+    }
+}
