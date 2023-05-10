f6b863ea1b59b19dd857523a5a6badee5693319f
Deniz Bineklioğlu <76698070+denizbineklioglu@users.noreply.github.com>
2023-05-10 16:29:18 +0300

    register

---
+++ EasyCash.Presentation/Controllers/RegisterController.cs
@@ -0,0 +1,45 @@
+﻿using EasyCash.DTO.Dtos.AppUserDtos;
+using EasyCash.Entity.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyCash.Presentation.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public RegisterController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(AppUserRegisterDto model)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser appUser = new AppUser()
+                {
+                    UserName = model.Username,
+                    Name = model.Name,
+                    Surname = model.Surname,
+                    Email = model.Email,
+                };
+
+                var result = await _userManager.CreateAsync(appUser, model.Password);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index","ConfirmMail");
+                }
+            }
+            return View();
+        }
+    }
+}
